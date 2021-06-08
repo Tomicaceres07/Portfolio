221dd973a30cf92d554143a8bea0b6eee9c608e1
Tomi <tomicaceres07@gmail.com>
2021-06-08 15:22:10 -0300

    Html Finished

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 let scrollY = window.pageYOffset;
 window.onscroll = function(){
     let check = document.getElementById('check');
+
     if(check.checked && (scrollY >= 0)){
         $('.nav_list').hide();
         $('#check').prop('checked', false);
