b1147b74f0e870ab93536f2be91d485c0fc269a5
Tomi <tomicaceres07@gmail.com>
2021-06-13 10:31:48 -0300

    Bootstrap added. Nav bar with transition at half height. Background added.

--- js/index.js
+++ js/index.js
@@ -1,27 +1,9 @@
-let scrollY = window.pageYOffset;
-window.onscroll = function(){
-    let check = document.getElementById('check');
-
-    if(check.checked && (scrollY >= 0)){
-        $('.nav_list').hide();
-        $('#check').prop('checked', false);
+$(window).scroll(function(){
+    height = $("#home").height();
+    half_height = height/2;
+    if ($("#nav").offset().top > (half_height)) {
+        $("#nav").addClass('background-rgba');
     }else{
-        $('.nav_list').show();
-    }
-}
-
-let check = document.getElementById('check');
-$(check).on('change', function(){
-    if (check) {
-        $('.icon-menu').addClass('spin-open');
-        $('.icon-menu').removeClass('spin-close');
+        $("#nav").removeClass('background-rgba');
     }
 });
-
-$(check).on('change', function(){
-    if (check.checked == false) {
-        $('.icon-menu').addClass('spin-close');
-        $('.icon-menu').removeClass('spin-open');
-    }
-});
-
