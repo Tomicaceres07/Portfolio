00527865605f1e388dbc75a75275b350e80c39be
Tomi <tomicaceres07@gmail.com>
2021-06-16 18:16:27 -0300

    Nav bar upgraded, Background Zoomed Out, some animations, change colors and alignments, button up added.

--- js/index.js
+++ js/index.js
@@ -1,18 +1,89 @@
-let scroll = window.pageYOffset;
-$(window).scroll(function(){
-    let actual_scroll = window.pageYOffset;
-    height = $("#home").height();
-    half_height = height/2;
-    if ($("#nav").offset().top > (half_height)) {
-        $("#nav").addClass('background-rgba');
-    }else{
-        $("#nav").removeClass('background-rgba');
-    }
+$(document).ready(function(){
 
-    if (scroll >= actual_scroll) {
-        document.getElementById('nav').style.top = '0';
-    }else{
-        document.getElementById('nav').style.top = '-100px';
-    }
-    scroll = actual_scroll;
+    let scroll = window.pageYOffset;
+
+    $(window).scroll(function(){
+        let actual_scroll = window.pageYOffset;
+        jq_home_height = $("#home").height();
+        half_height = jq_home_height/2;
+        nav = $("#nav");
+        if (nav.offset().top > (half_height)) {
+            nav.addClass('background-rgba');
+        }else{
+            nav.removeClass('background-rgba');
+        }
+        aria_expanded = document.getElementById('toggle').getAttribute('aria-expanded');
+        if (scroll >= actual_scroll) {
+            document.getElementById('nav').style.top = '0';
+            if(aria_expanded == 'true'){
+                $('#menu').slideDown();
+            }
+        }else{
+            document.getElementById('nav').style.top = '-100px';
+            $('#menu').slideUp();
+            // $("#toggle").attr("aria-expanded","false");
+        }
+        scroll = actual_scroll;
+
+        let window_height = window.innerHeight;
+
+        let home = document.getElementById('home');
+        let about = document.getElementById('about');
+        let portfolio = document.getElementById('portfolio');
+        let contact = document.getElementById('contact');
+
+        let home_height = home.getBoundingClientRect().bottom;
+        let about_height = about.getBoundingClientRect().top;
+        let portfolio_height = portfolio.getBoundingClientRect().top;
+        let contact_height = contact.getBoundingClientRect().top;
+
+        if(home_height >= 110){
+            $('#li_home').addClass('active').siblings().removeClass('active');
+        }
+        if (about_height <= 0) {
+            $('#li_about').addClass('active').siblings().removeClass('active');
+        }
+        if (portfolio_height <= 0) {
+            $('#li_portfolio').addClass('active').siblings().removeClass('active');
+        }
+        if (contact_height <= window_height) {
+            $('#li_contact').addClass('active').siblings().removeClass('active');
+        }
+
+    });
+
+
+    $('#up').on('click', function(){
+        $('html, body').animate({
+            scrollTop: 0
+        }, 500);
+    });
+
+    $('#logo').on('click', function(){
+        $('html, body').animate({
+            scrollTop: 0
+        }, 500);
+    });
+
 });
+
+
+
+
+
+/* $('#toggle').click(function(){
+    $('#menu').toggle();
+    aria_expanded = document.getElementById('toggle').getAttribute('aria-expanded');
+    if(aria_expanded == 'true'){
+        console.log('aria-expanded es TRUE')
+        $('#menu').slideUp();
+        // $("#toggle").attr("aria-expanded","false");
+        // console.log($("#toggle").attr("aria-expanded","false"));
+    }else if(aria_expanded == 'false'){
+        $('#menu').slideDown();
+    }
+    console.log(aria_expanded);
+}); */
+
+
+
