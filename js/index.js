7306eaec421ffcb2bd76f6ea3a2824dbef914948
Tomi <tomicaceres07@gmail.com>
2022-12-10 12:54:46 -0300

    Navbar spanish fixed

--- js/index.js
+++ js/index.js
@@ -6,11 +6,7 @@ $(document).ready(function(){
 
     $(window).scroll(function(){
         let actual_scroll = window.pageYOffset;
-        jq_home_height = $("#home").height();
-        half_height = jq_home_height/2;
         nav = $("#nav");
-
-        aria_expanded = document.getElementById('toggle').getAttribute('aria-expanded');
 
         scroll = actual_scroll;
 
@@ -23,60 +19,101 @@ $(document).ready(function(){
         let portfolio = document.getElementById('portfolio');
         let contact = document.getElementById('contact');
 
+        let home_spa = document.getElementById('home-spa');
+        let about_spa = document.getElementById('about-spa');
+        let portfolio_spa = document.getElementById('portfolio-spa');
+        let contact_spa = document.getElementById('contact-spa');
+
         let home_height = home.getBoundingClientRect().bottom;
         let about_height = about.getBoundingClientRect().top;
         let portfolio_height = portfolio.getBoundingClientRect().top;
         let contact_height = contact.getBoundingClientRect().bottom;
         let contact_height_top = contact.getBoundingClientRect().top;
 
+        let home_height_spa = home_spa.getBoundingClientRect().bottom;
+        let about_height_spa = about_spa.getBoundingClientRect().top;
+        let portfolio_height_spa = portfolio_spa.getBoundingClientRect().top;
+        let contact_height_spa = contact_spa.getBoundingClientRect().bottom;
+        let contact_height_top_spa = contact_spa.getBoundingClientRect().top;
+
         // HOME
         if(home_height >= 110){
-            $('#li_home').addClass('active').siblings().removeClass('active');
+            $('.li_home').addClass('active').siblings().removeClass('active');
+        }
+
+        // HOME SPA
+        if(home_height_spa >= 110){
+            $('.li_home-spa').addClass('active').siblings().removeClass('active');
         }
 
         // ABOUT
         if (about_height <= 0) {
-            $('#li_about').addClass('active').siblings().removeClass('active');
+            $('.li_about').addClass('active').siblings().removeClass('active');
         }
         if (window_one_third >= about_height) {
-            $('#row-about').addClass('animate__animated animate__fadeInDown');
+            $('.row-about').addClass('animate__animated animate__fadeInDown');
+        }
+
+        // ABOUT SPA
+        if (about_height_spa <= 0) {
+            $('.li_about-spa').addClass('active').siblings().removeClass('active');
+        }
+        if (window_one_third >= about_height_spa) {
+            $('.row-about').addClass('animate__animated animate__fadeInDown');
         }
 
         // PORTFOLIO
         if (portfolio_height <= 0) {
-            $('#li_portfolio').addClass('active').siblings().removeClass('active');
+            $('.li_portfolio').addClass('active').siblings().removeClass('active');
         }
         if (window_one_third >= portfolio_height) {
             $('.row-portfolio').addClass('animate__animated animate__fadeIn');
         }
 
+        // PORTFOLIO SPA
+        if (portfolio_height_spa <= 0) {
+            $('.li_portfolio-spa').addClass('active').siblings().removeClass('active');
+        }
+        if (window_one_third >= portfolio_height_spa) {
+            $('.row-portfolio').addClass('animate__animated animate__fadeIn');
+        }
+
         // CONTACT
         if (contact_height <= window_height) {
-            $('#li_contact').addClass('active').siblings().removeClass('active');
+            $('.li_contact').addClass('active').siblings().removeClass('active');
         }
-        // console.log('woh '+window_one_third);
-        // console.log('ch '+contact_height);
         if (window_one_third >= contact_height_top) {
+            $('.row-contact').addClass('animate__animated animate__fadeInUp');
+        }
+
+        // CONTACT SPA
+        if (contact_height_spa <= window_height) {
+            $('.li_contact-spa').addClass('active').siblings().removeClass('active');
+        }
+        if (window_one_third >= contact_height_top_spa) {
             $('.row-contact').addClass('animate__animated animate__fadeInUp');
         }
 
     });
 
 
-    $('#up').on('click', function(){
+    $('.up').on('click', function(){
         $('html, body').animate({
             scrollTop: 0
         }, 500);
     });
 
 
-    $('#down').on('click', function(){
+    $('.down').on('click', function(){
         $('html, body').animate({
             scrollTop: about_height
         }, 500);
+        $('html, body').animate({
+            scrollTop: about_height_spa
+        }, 500);
     });
 
-    $('#logo').on('click', function(){
+    $('.logo').on('click', function(){
         $('html, body').animate({
             scrollTop: 0
         }, 500);
